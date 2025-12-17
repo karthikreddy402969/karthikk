38b8f1e273bdaeaa5b3c322ad33a35cc46c41e81
karthikreddy402969 <karthikreddy402969@gmail.com>
2025-12-17 11:51:02 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+from pathlib import Path
+
+st.set_page_config(
+    page_title="stydent Task Dashboard",
+    layout="wide"
+
+)
+
+html_file=Path("index.html").read_text(encoding="utf-8")
+
+st.components.v1.html(html_file,height=900,scrolling=True)
